ef071a35f18262f07471083138758cc02ddcd5a8
DylanS97 <67909829+DylanS97@users.noreply.github.com>
2021-06-01 13:35:15 +0100

    Added shadow when menu is open on mobile. Structure changes. Added a favicon. Added validation to form fields.

--- js-src/app.js
+++ js-src/app.js
@@ -3,12 +3,13 @@
 const length = document.querySelector('#typed-header > p').textContent.length;
 const typeSpeed = 35;
 const waitTime = ((typeSpeed + 10) * length);
+$('#typed-for-header').html("");
 var typed = new Typed('#typed-for-header', {
     stringsElement: '#typed-header',
     typeSpeed: typeSpeed,
     showCursor: false
 });
-setTimeout(function() {
+setTimeout(() => {
     var typed = new Typed('#typed-for-sub', {
         stringsElement: '#typed-sub',
         typeSpeed: typeSpeed,
@@ -17,17 +18,57 @@ setTimeout(function() {
 }, waitTime);
 
 // Open and Close Navigation
-$('.initial').on('click', function() {
+$('.initial').on('click', () => {
+    $('body').css({overflowY: "hidden"});
     $('.initial').hide();
     const $nav = $('.nav-container');
     const $close = $('.close-nav');
+    const $overlay = $('.overlay');
+    $overlay.addClass('overlay-show');
     $nav.addClass('nav-show');
     $close.addClass('close-nav-show').width(30);
-    $('.close-nav').on('click', function() {
+    $('.close-nav').on('click', () => {
+        $('body').css({overflowY: "scroll"});
+        $overlay.removeClass('overlay-show');
         $nav.removeClass('nav-show');
         $close.removeClass('close-nav-show').width(0);
-        setTimeout(function() {
+        setTimeout(() => {
             $('.initial').show(200);
         }, 150);
     });
+    $('.overlay').on('click', () => {
+        $('body').css({overflowY: "scroll"});
+        $overlay.removeClass('overlay-show');
+        $nav.removeClass('nav-show');
+        $close.removeClass('close-nav-show').width(0);
+        setTimeout(() => {
+            $('.initial').show(200);
+        }, 150);
+    });
+});
+
+// Form Validation
+$('.submit-btn').on('click', () => {
+    let empty = false;
+
+    // Array that holds all of the input fields.
+    let fields = [];
+    fields[0] = $('.first-name');
+    fields[1] = $('.last-name');
+    fields[2] = $('.email-addr');
+    fields[3] = $('.email-subject');
+    fields[4] = $('.email-msg');
+
+    // Checks if any of the input fields in the array are empty and changes the colour of empty fields to red.
+    for (let i = 0; i < 5; i++) {
+        if (fields[i].val() === "") {
+            fields[i].addClass('error-col');
+            empty = true;
+        }
+    }
+
+    // Do nothing if any are empty.
+    if (empty) {
+        return;
+    }
 });
---
+++ js/app.js
@@ -0,0 +1,77 @@
+"use strict";
+
+// Typing Effect
+var length = document.querySelector('#typed-header > p').textContent.length;
+var typeSpeed = 35;
+var waitTime = (typeSpeed + 10) * length;
+$('#typed-for-header').html("");
+var typed = new Typed('#typed-for-header', {
+  stringsElement: '#typed-header',
+  typeSpeed: typeSpeed,
+  showCursor: false
+});
+setTimeout(function () {
+  var typed = new Typed('#typed-for-sub', {
+    stringsElement: '#typed-sub',
+    typeSpeed: typeSpeed,
+    showCursor: false
+  });
+}, waitTime); // Open and Close Navigation
+
+$('.initial').on('click', function () {
+  $('body').css({
+    overflowY: "hidden"
+  });
+  $('.initial').hide();
+  var $nav = $('.nav-container');
+  var $close = $('.close-nav');
+  var $overlay = $('.overlay');
+  $overlay.addClass('overlay-show');
+  $nav.addClass('nav-show');
+  $close.addClass('close-nav-show').width(30);
+  $('.close-nav').on('click', function () {
+    $('body').css({
+      overflowY: "scroll"
+    });
+    $overlay.removeClass('overlay-show');
+    $nav.removeClass('nav-show');
+    $close.removeClass('close-nav-show').width(0);
+    setTimeout(function () {
+      $('.initial').show(200);
+    }, 150);
+  });
+  $('.overlay').on('click', function () {
+    $('body').css({
+      overflowY: "scroll"
+    });
+    $overlay.removeClass('overlay-show');
+    $nav.removeClass('nav-show');
+    $close.removeClass('close-nav-show').width(0);
+    setTimeout(function () {
+      $('.initial').show(200);
+    }, 150);
+  });
+}); // Form Validation
+
+$('.submit-btn').on('click', function () {
+  var empty = false; // Array that holds all of the input fields.
+
+  var fields = [];
+  fields[0] = $('.first-name');
+  fields[1] = $('.last-name');
+  fields[2] = $('.email-addr');
+  fields[3] = $('.email-subject');
+  fields[4] = $('.email-msg'); // Checks if any of the input fields in the array are empty and changes the colour of empty fields to red.
+
+  for (var i = 0; i < 5; i++) {
+    if (fields[i].val() === "") {
+      fields[i].addClass('error-col');
+      empty = true;
+    }
+  } // Do nothing if any are empty.
+
+
+  if (empty) {
+    return;
+  }
+});
